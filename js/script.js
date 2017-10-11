a6870070241613e9300cfa4c56a355ec4949637e
Scott <scottdlivingstone@gmail.com>
2017-10-10 21:49:16 -0700

    modify smooth scroll function to account for header height, fix form input width

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
 //Smooth Scroll from https://css-tricks.com/snippets/jquery/smooth-scrolling/
+//Updated to account for fixed header height
+
 // Select all links with hashes
+
+
 $(function () {
-  $("a[href*='#']:not([href='#'])").click(function () {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
+    $("a[href*='#']:not([href='#'])").click(function () {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        var headerHeight = $("header").height();
+        var scrollToPosition = $(target).offset().top - headerHeight;
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: scrollToPosition}, 800);
+          return false;
+        }
       }
-    }
-  });
+    });
 });
+
