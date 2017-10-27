76f387c1230f2d57031f09606d5ee40f5d540a85
Scott <scottdlivingstone@gmail.com>
2017-10-27 13:06:02 -0700

    make accessibilty changes

--- js/script.js
+++ js/script.js
@@ -86,6 +86,13 @@ $(function () {
       }
     };
 
+    // Fix "Skip Link" Focus in Webkit
+    $(function () {
+      $("a[href^='#']").not("a[href='#']").click(function () {
+        $("#" + $(this).attr("href").slice(1) + "").focus();
+      });
+    });
+
 
 
 
