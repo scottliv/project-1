3bfa73427f6e4fbf57f38d742478da111f75d177
Scott <scottdlivingstone@gmail.com>
2017-11-27 13:54:40 -0800

    clean up css and javascript files fix background image sizes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,42 @@
 
 $(function () {
-  //Smooth Scroll from https://css-tricks.com/snippets/jquery/smooth-scrolling/
-  //Updated to account for fixed header height
+
+  // Function to apply different jQuery depending on screen size.. not strictly necessary in this case, but a fun exercise
+  function checkSize() {
+    // Checks for a style that doesn't exist in the mobile section
+    if ($(".mobile-hide").css("display") == "block") {
+      $('.main-carousel').flickity({
+        cellAlign: 'left',
+        pageDots: true,
+        prevNextButtons: false,
+        contain: true,
+        wrapAround: true,
+      });
+    }
+    else {
+      $('.main-carousel').flickity({
+        accessibility: true,
+        cellAlign: 'center',
+        contain: true,
+        pageDots: true,
+        prevNextButtons: false,
+      });
+    }
+  }
+
+  // Email Validation
+  function validateEmail(email) {
+    var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    if (filter.test(email)) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  // Smooth Scroll from https://css-tricks.com/snippets/jquery/smooth-scrolling/
+  // Updated to account for fixed header height
   // Select all links with hashes
     $("a[href*='#']:not([href='#'])").click(function () {
       if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -24,24 +59,21 @@ $(function () {
       $('.cart-counter').text(counter);
       $('.cart-counter').css("display", "block");
     });
-
-    // Flickity 
-    checkSize();
-    // An attempt to throttle the checkSize function
-
-    var checkingSize = false;
-
+ 
+    // Call checksize once to set up flickity and whenever the window is resized, 
+    // throttled to prevent large amounts of function calls
+    checkSize()
     $(window).resize(function(){
       setTimeout(function(){
         checkSize()
       },100);
     });
 
-    // Sign up form
+    // Sign up form email validation
     $(".form-button").on('click', function(event){
       event.preventDefault();
-      var sEmail = $('#subscribe').val();
-      if (validateEmail(sEmail)) {
+      var email = $('#subscribe').val();
+      if (validateEmail(email)) {
         alert('Thanks for subscribing.');
       }
       else {
@@ -50,48 +82,12 @@ $(function () {
     })
 });
 
-  // Function to apply different jQuery depending on screen size.. not strictly necessary in this case, but a fun exercise
-    function checkSize() {
-      // Checks for a style that doesn't exist in the mobile section
-      if ($(".mobile-hide").css("display") == "block") {
-        // console.log("resize!")
-        $('.main-carousel').flickity({
-          cellAlign: 'left',
-          pageDots: true,
-          prevNextButtons: false,
-          contain: true,
-          wrapAround: true,
-        });
-      }
-      else {
-        // console.log("mobile!")
-        $('.main-carousel').flickity({
-          accessibility: true,
-          cellAlign: 'center',
-          contain: true,
-          pageDots: true,
-          prevNextButtons: false,
-        });
-      }
-    }
-
-    // Email Validation
-    function validateEmail(sEmail) {
-      var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-      if (filter.test(sEmail)) {
-        return true;
-      }
-      else {
-        return false; 
-      }
-    };
-
-    // Fix "Skip Link" Focus in Webkit
-    $(function () {
-      $("a[href^='#']").not("a[href='#']").click(function () {
-        $("#" + $(this).attr("href").slice(1) + "").focus();
-      });
-    });
+ // Fix "Skip Link" Focus in Webkit
+ $(function () {
+   $("a[href^='#']").not("a[href='#']").click(function () {
+     $("#" + $(this).attr("href").slice(1) + "").focus();
+   });
+ });
 
 
 
